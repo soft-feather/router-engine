f2861aafe9e0d56df0331ba2fa42c7c5ed66519f
songda <1025098201@qq.com>
2023-07-28 17:08:49 +0800

    feat: 增加路由表管理模块

---
+++ module/route/table/manager/route_table_manager.go
@@ -0,0 +1,14 @@
+package manager
+
+type Service struct {
+}
+
+func (s *Service) Init() error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *Service) Shutdown() {
+	//TODO implement me
+	panic("implement me")
+}
